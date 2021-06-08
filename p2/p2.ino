225eb9089145482a9cee8863b3253973a4225e88
Aranmarh <aranmarh@gmail.com>
2021-06-07 20:28:00 -0600

    se agrego la progra del sensor

--- p2/p2.ino
+++ p2/p2.ino
@@ -22,6 +22,16 @@ void setup() {
 void loop() {
 
   cm = 0.01723 * distancia(6, 6);
+  cm2 = 0.01723 * distancia(29, 29);
+  if(cm<150){
+     int x = 150 - cm;
+     //se sube  el motor derecho
+    }
+
+  if(cm2<150){
+      int y = 150-cm2;
+      //se sube el motor izquierdo
+    }
   // put your main code here, to run repeatedly:
 
 }
